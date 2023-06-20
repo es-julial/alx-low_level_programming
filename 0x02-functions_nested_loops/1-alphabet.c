95407492b0a1afaf22f0cc8490e908adc4d39f5a
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-06-20 04:48:02 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ * Description: Prints alphabets in lower case
+ * Return: (0)
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
